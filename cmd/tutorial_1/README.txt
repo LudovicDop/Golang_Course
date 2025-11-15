45197433c56e80dfa17d35566c1795df8675c7b5
Loacking <LudovicDop@gmail.com>
2025-11-15 19:01:46 +0100

    .

--- cmd/tutorial_1/README.txt
+++ cmd/tutorial_1/README.txt
@@ -1,1 +1,1 @@
-https://www.youtube.com/watch?v=8uiZC0l4Ajw (35:36)
+https://www.youtube.com/watch?v=8uiZC0l4Ajw (40:25)
--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+var wg = sync.WaitGroup{}
+var mutex = sync.Mutex{}
+var fakeDb []string = []string{"user1", "user2", "user3", "user4", "user5", "user6"}
+var result []string
+
 func main() {
-	var ptr *int32 = new(int32)
-	var ptr2
-	*ptr = 42
-	fmt.Printf("address ptr: %v and value %v", ptr, *ptr)
+
+	t0 := time.Now()
+
+	for i := range fakeDb {
+		wg.Add(1)
+		go dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("The result is: %v", result)
+	fmt.Printf("\n\x1b[33mTotal execution time: %v\x1b[0m", time.Since(t0))
+}
+
+func dbCall(i int) {
+	// var delay float32 = rand.Float32() * 3
+
+	// fmt.Printf("delay: %v\n", delay)
+	time.Sleep(time.Duration(3) * time.Second)
+	fmt.Printf("The result is: %v\n", i)
+	mutex.Lock()
+	result = append(result, fakeDb[i])
+	mutex.Unlock()
+	wg.Done()
 
 }
